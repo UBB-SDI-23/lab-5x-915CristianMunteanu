3f9167f9454b230c9d0f004a30af124dcb7e52b1
Munteanu Cristian <cristian.munteanu@stud.ubbcluj.ro>
2023-05-03 16:33:55 +0300

    modified Frontend

--- vite-project/src/constants.ts
+++ vite-project/src/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_API_URL = "http://localhost:8080";
+export const BACKEND_API_URL = "https://sdiapi-385516.lm.r.appspot.com";
 export const SHOW_NOTIFICATION = "SHOW_NOTIFICATION";
 export const ERROR_MESSAGE = "An error has occured!";
 export const SEVERITY_SUCCESS = "success";
